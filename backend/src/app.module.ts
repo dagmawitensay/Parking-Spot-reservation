ef54c6b5a316b59d2faac6a6c24ed06b250333a4
dagiTensay <dagitensay@gmail.com>
2023-05-06 12:26:30 +0300

    initalize backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ParkingSpotModule } from './parking-spot/parking-spot.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ParkingSpotModule, ReservationModule],
   controllers: [AppController],
   providers: [AppService],
 })

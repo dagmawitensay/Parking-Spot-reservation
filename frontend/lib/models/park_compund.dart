ed97b3a6e2cabb89e5a497f0d15747e2f97d7a09
Nathnael Yimer <106149662+natnaelyimer2040@users.noreply.github.com>
2023-05-23 13:44:42 +0300

    Create park_compund.dart
    this is the file that will be updated because it is diffrent from the back end

---
+++ frontend/lib/models/park_compund.dart
@@ -0,0 +1,16 @@
+class ParkCompound {
+  final String name;
+  final String address;
+  final double latitude;
+  final double longitude;
+  final double price;
+  final String image;
+
+  ParkCompound(
+      {required this.name,
+      required this.address,
+      required this.latitude,
+      required this.longitude,
+      required this.price,
+      required this.image});
+}

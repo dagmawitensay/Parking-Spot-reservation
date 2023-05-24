e58dab28b11930bd1ff1fec650fe5a3f2a6bbb24
Ephrem-shimels21 <eph21sz@gmail.com>
2023-05-25 01:22:38 +0300

    create dto for the signup

---
+++ backend/auth/common_dto/common_dto.dto.ts
@@ -0,0 +1,28 @@
+import {IsEmail, IsEmpty, IsNotEmpty, IsString} from  'class-validator';
+export class Dto{
+    
+    @IsNotEmpty()
+    first_name: string;
+    
+    @IsNotEmpty()
+    last_name: string;
+    
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
+    email:string;
+
+    // @IsString()
+    // role:string;
+    
+    @IsNotEmpty()
+    phone_no:string;
+
+    @IsString()
+    @IsNotEmpty()
+    password:string;
+
+}
+
+
+   

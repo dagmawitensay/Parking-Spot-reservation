0c6bd3473faf0731842f0125cbfc782cace4e55c
Ephrem-shimels21 <eph21sz@gmail.com>
2023-05-25 01:23:34 +0300

    create dto for signin

---
+++ backend/auth/decorator/role.enum.ts
@@ -0,0 +1,4 @@
+export enum Role{
+    owner = 'owner',
+    reserver = 'reserver'
+} 
---
+++ backend/auth/decorator/roles.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from "@nestjs/common";
+import {Role} from "./role.enum";
+
+export const Roles = (...roles:Role[]) =>SetMetadata('roles',roles);
+
+

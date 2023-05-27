46bda70272e0269fac67088f3ca725913135972d
dagiTensay <dagitensay@gmail.com>
2023-05-27 20:10:40 +0300

    add bloc manager

---
+++ frontend/lib/compounds/bloc/compound_event.dart
@@ -0,0 +1,51 @@
+import 'package:equatable/equatable.dart';
+
+import '../models/compound.dart';
+
+abstract class CompoundEvent extends Equatable {
+  const CompoundEvent();
+}
+
+class CompoundCreate extends CompoundEvent {
+  final Compound compound;
+
+  const CompoundCreate(this.compound);
+
+  @override
+  List<Object> get props => [Compound];
+
+  @override
+  String toString() => 'Compound Created {compound: ${compound.id}}';
+}
+
+class CompoundLoad extends CompoundEvent {
+  const CompoundLoad();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CompoundUpdate extends CompoundEvent {
+  final int id;
+  final Compound compound;
+
+  const CompoundUpdate(this.id, this.compound);
+
+  @override
+  List<Object> get props => [Compound];
+
+  @override
+  String toString() => 'Compound Updated {compound: ${compound.id}}';
+}
+
+class CompoundDelete extends CompoundEvent {
+  final int id;
+
+  const CompoundDelete(this.id);
+
+  @override
+  List<Object> get props => [id];
+
+  @override
+  toString() => 'Compound Deleted {compound Id: $id}';
+}

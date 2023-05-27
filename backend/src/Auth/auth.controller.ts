f5ef555bd1285637bc1304ab7c4067f91eb28d99
Ephrem-shimels21 <eph21sz@gmail.com>
2023-05-27 20:11:01 +0300

    modify the authcontroller to correct errors

--- backend/src/Auth/auth.controller.ts
+++ backend/src/Auth/auth.controller.ts
@@ -21,12 +21,13 @@ export class AuthController{
     Parker_signup(@Body() dto:Dto){
         return this.authservice.spotUserSignup(dto);
     }
-
+    @Roles(Role.owner)
+    @UseGuards(JwtGuard, RolesGuard)
     @Post('signin')
     signin(@Body() dto: AuthDto){
         return this.authservice.Signin(dto);
     }
-    @Roles(Role.owner)
+    @Roles(Role.reserver)
     @UseGuards(JwtGuard, RolesGuard)
     @Get('check')
     for_checking(){
